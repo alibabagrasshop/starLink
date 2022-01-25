4d26ae8c05aa7a6e9ab1d14e441da6e440992f91
alibabagrasshop <58092865+alibabagrasshop@users.noreply.github.com>
2022-01-24 21:46:57 -0800

    Add files via upload
    starLink project

---
+++ src/components/constants.js
@@ -0,0 +1,8 @@
+export const SAT_API_KEY = "VQ5UUJ-RT6KFZ-949RH6-4T0A";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
